f9f9b6de40b0ba94ec8982e5c2d93b8b41d549e4
varun <varunannepu123@gmail.com>
2023-12-07 23:51:27 +0530

    vpc

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "components" {
   source              = "git::https://github.com/Annepuvarunkumar/tf-module-vpc.git"
 
   for_each    = var.vpc
-  cidr        = each.value[cidr]
-  subnets     = each.value[subnets]
+  cidr        = each.value["cidr"]
+  subnets     = each.value["subnets"]
 }
 
 
