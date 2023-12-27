a96c30cf97d52aaca5b2b97e4e043e1daff8c730
varun <varunannepu123@gmail.com>
2023-12-27 20:55:13 +0530

    docdb cluster

--- locals.tf
+++ locals.tf
@@ -2,4 +2,8 @@ locals {
   vpc_id       = lookup(lookup(module.vpc, "main", null ), "vpc_id", null)
   app_subnets  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "app", null), "subnet_ids", null): v.id]
   db_subnets  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "db", null), "subnet_ids", null): v.id]
+
+ #cidr....we allow only for app subnets.
+  app_subnets_cidr  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "app", null), "subnet_ids", null): v.cidr_block]
+
 }
