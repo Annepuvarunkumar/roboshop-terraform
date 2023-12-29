c932f42868aa88a5081c268b9f705871456d1de0
varun <varunannepu123@gmail.com>
2023-12-29 18:10:18 +0530

    rds cluster

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   app_subnets  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "app", null), "subnet_ids", null): v.id]
   db_subnets  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "db", null), "subnet_ids", null): v.id]
 
- #cidr....we allow only for app subnets.
+ #cidr....we allow Databases to enter only for the app subnets.
   app_subnets_cidr  = [for k,v in lookup(lookup(lookup(lookup(module.vpc, "main", null ), "subnets", null), "app", null), "subnet_ids", null): v.cidr_block]
 
 }
